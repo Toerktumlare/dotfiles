5055b17f1da2b04932bcdc2c9a9c41db65a38832
Andolf Thomas Konsult <Konsult.Thomas.Andolf@lansforsakringar.se>
2020-01-24 13:45:06 +0100

    first draft of windows vscode setup scripts

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿Set-Location C:\Users\B926FT
+
+$ENV:PATH="C:\Program Files\Microsoft VS Code\bin;$ENV:PATH"
+
+# Load scripts from autoload dir
+Get-ChildItem "Documents/WindowsPowerShell/autoload/*.ps1" | ForEach-Object{
+    .$_
+}
