7dac1b121d9731f233e16663018a33559cf0a575
Thomas Andolf <thomas.andolf@truesec.se>
2021-02-11 17:26:25 +0100

    updated path in fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,7 @@ alias ls='exa'
 alias ll='exa -laF'
 alias vim='nvim'
 
- if test -e /usr/bin/dircolors
-     test -r ~/.dircolors && eval (dircolors -c ~/.dircolors) || eval (dircolors -c)
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
- end
-
  # display full directory names in prompt
  set fish_prompt_pwd_dir_length 0
+
+ set PATH $PATH /bin/snap
