da61bf488f203408a3e630db45bba6530753214b
Thomas Andolf <thomas.andolf@truesec.se>
2021-02-12 15:15:23 +0100

    added ls/exa in fish config, and update Xresources

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,13 @@
-alias ls='exa'
-alias ll='exa -laF'
-alias vim='nvim'
+# check if exa exists otherwise use ls
+if command -v exa > /dev/null
+    abbr -a ls 'exa'
+    abbr -a ll 'exa -laF'
+else
+    abbr -a l 'ls'
+    abbr -a ll 'ls -laF'
+end
+
+abbr -a vim 'nvim'
 
  # display full directory names in prompt
  set fish_prompt_pwd_dir_length 0
